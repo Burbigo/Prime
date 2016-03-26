ee8c95a14b8c8efa3104553c92af6b186884a549
okvak <vadim-0497@rambler.ru>
2016-03-26 16:27:00 +0200

    Update insert devices.sql

--- SQL/insert devices.sql
+++ SQL/insert devices.sql
@@ -1,8 +1,8 @@
 use prime
- insert into devices values
- ('dell','server',1,'198.231.1.2','operating','october 29,2010','new->operating','september 23,2011 14:46:32'),
- ('epson','printer',2,'121.112.1.3','new','november 06,2013','',''),
- ('linksys','router',1,'198.231.1.1','repairing','may 12,2014','operating->repairing','february 28,2016 15:35:04'),
- ('hp','computer',2,'121.112.1.2','operating','march 16,2015','repairing->operating','october 05,2015 09:51:22')
+insert into devices values
+ ('dell','server','198.231.1.2',1,'operating','october 29,2010','new->operating','september 23,2011 14:46:32'),
+ ('epson','printer','121.112.1.3',2,'new','november 06,2013','','november 06,2013'),
+ ('linksys','router','198.231.1.1',1,'repairing','may 12,2014','operating->repairing','february 28,2016 15:35:04'),
+ ('hp','computer','121.112.1.2',2,'operating','march 16,2015','repairing->operating','october 05,2015 09:51:22')
 
 select*from devices
