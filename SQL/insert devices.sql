5128ecac80a6110c09be78449af46e26b9127056
okvak <vadim-0497@rambler.ru>
2016-04-26 23:49:25 +0300

    Delete insert devices.sql

--- SQL/insert devices.sql
+++
@@ -1,8 +0,0 @@
-use prime
-insert into devices values
- ('dell','server','198.231.1.2',1,'operating','october 29,2010','new->operating','september 23,2011 14:46:32'),
- ('epson','printer','121.112.1.3',2,'new','november 06,2013','','november 06,2013'),
- ('linksys','router','198.231.1.1',1,'repairing','may 12,2014','operating->repairing','february 28,2016 15:35:04'),
- ('hp','computer','121.112.1.2',2,'operating','march 16,2015','repairing->operating','october 05,2015 09:51:22')
-
-select*from devices
